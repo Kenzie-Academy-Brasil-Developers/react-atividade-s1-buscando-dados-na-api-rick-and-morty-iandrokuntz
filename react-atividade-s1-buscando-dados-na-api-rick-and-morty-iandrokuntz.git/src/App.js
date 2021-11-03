c876a4c76b348b139782f1b10e56e3547a5f8ebc
iandrokuntz <ianbertoluchi99@gmail.com>
2021-11-03 13:14:40 -0300

    atividade pronta

---
+++ react-atividade-s1-buscando-dados-na-api-rick-and-morty-iandrokuntz.git/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import Characters from './components/Characters';
+import './App.css';
+
+function App() {
+
+  const [characterList, setCharacterList] = useState([])
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+    .then((response) => response.json())
+    .then((response) => setCharacterList(response.results))
+    .catch((err) => console.log(err))
+  }, [])
+
+  return (
+
+    <div className="App">
+      
+      <Characters characterList={characterList}/>
+
+    </div>
+  );
+}
+
+export default App;
